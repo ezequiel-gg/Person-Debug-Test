f2c8cba77a5eaab577f6ba7b9fe82d5b1de2c461
ezequiel-gg <54220485+ezequiel-gg@users.noreply.github.com>
2020-09-12 22:47:27 -0300

    finished

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,16 +6,53 @@ namespace Tests
 {
     public class Tests
     {
+
+        private Person person;
+
+
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+            person = new Person("Nombre", "5.286.500-6");
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void Person_Test()
         {
-            // Insertá tu código  de pruebaaquí
+            this.person = new Person("Nombre", "5.286.500-6");
+            Assert.IsNotNull(this.person.ID);
+            Assert.IsNotNull(this.person.Name);
+        }
+
+        [Test]
+        public void Doc_Clean_Test()
+        {
+            this.person = new Person("Nombre", "52865006");
+            Assert.IsNotNull(this.person.ID);
+            Assert.IsNotNull(this.person.Name);
+        }
+        [Test]
+        public void Doc_Dot_Test()
+        {
+            this.person = new Person("Nombre", "5.286.5006");
+            Assert.IsNotNull(this.person.ID);
+            Assert.IsNotNull(this.person.Name);
+        }
+
+        [Test]
+        public void Doc_Character_Test()
+        {
+            this.person = new Person("Nombre", "5-286-500-6");
+            Assert.IsNotNull(this.person.ID);
+            Assert.IsNotNull(this.person.Name);
+        }
+
+        [Test]
+        public void Empty_Test()
+        {
+            this.person = new Person("", "");
+            Assert.IsNull(this.person.ID);
+            Assert.IsNull(this.person.Name);
         }
     }
 }
